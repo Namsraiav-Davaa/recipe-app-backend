8bb2604fdee2eff99faffbfddd2d23b82d0f5352
Namsraijav <Namsraijav.d@databank.mn>
2024-03-25 14:57:06 +0800

    lesson 36 done

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add (x, y):
+    """add x and y return result."""
+    return x+y
+
+def subtract (x, y):
+    """Subtract x from y return result."""
+    return y - x
