e5beb166a8834bf7db14466b23109d0b3c1a2eb0
Namsraijav <Namsraijav.d@databank.mn>
2024-03-25 17:27:49 +0800

    aa

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,4 @@
-"""
-Django command to wait for database to be available 
-"""
+""" Django command to wait for database to be available """
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
